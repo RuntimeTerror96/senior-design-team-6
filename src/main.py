31cb3bcd241b322ab526e98668ca815fdc73fb87
Hermeskid123 <preston.mann@mavs.uta.edu>
2022-09-26 13:29:03 -0500

    added a main.py and a test.py

---
+++ src/main.py
@@ -0,0 +1,34 @@
+#============================================================
+#
+#                         HTTP 418
+#               CSE 4316/4317 - Senior Design
+#            The University of Texas at Arlington
+#    Dylan Bryce, Biraj GC, Preston Mann, Isabel Metevier
+#
+#------------------------------------------------------------
+#
+# File: doxygen-example.py
+#
+# Brief: This file is our main it will run our program 
+#        Model will not be trained in this file 
+#        
+#
+# Revision History:
+# Date       | Engineer     | Description
+# -----------|--------------|--------------------------------
+# 09-26-2022 | Mann, P.    | Initial Release
+#
+#============================================================
+
+import sys 
+import numpy as np
+
+print("python version: ",sys.version)
+print("numpy version: ",np.__version__)
+
+
+def main():
+    print("\nmain is running")
+
+if __name__ == "__main__":
+    sys.exit(main())
